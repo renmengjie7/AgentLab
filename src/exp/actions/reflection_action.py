e2d56dec737ac3f0587e9c2465021c55e09b4d7c
任梦婕 <mjren@bupt.cn>
2023-04-20 18:35:26 +0800

    modify reflection function params

--- src/exp/actions/reflection_action.py
+++ src/exp/actions/reflection_action.py
@@ -10,7 +10,16 @@ class ReflectionAction(BaseAction):
     def __init__(self, expe_info: ExpeInfo):
         super().__init__(expe_info)
 
-    def run(self, *args, **kwargs):
+    def run(self, num: int,
+            input: bool=False,
+            output: bool=True,
+            prompt: str="经过最近的阅读, 你觉得自己的profile有何变化? 以下是你的阅读内容:\n",
+            *args, **kwargs, ):
+        """
+        :param input 记忆流中的input内容 (对话的输入)
+        :param output 记忆流中的output内容 (对话的输出)
+        对最近的多少条进行反思? 
+        """
         # 一个简单的反思
         for agent in self.expe_info.get_agent_ids():
-            recent_memory = self.expe_info.agents[agent].memory.retrive_by_recentness(100)
+            recent_memory = self.expe_info.agents[agent].memory.retrive_by_recentness(num)
