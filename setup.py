4c5d1701d8f1df04cb08cece357ffb8ae0202be5
GuoShiguang <guoshiguang0303@gmail.com>
2023-04-25 22:34:41 +0800

    add __init__.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages  # 这个包没有的可以pip一下
 
 setup(
     name="AISimuToolKit",  # 这里是pip项目发布的名称
-    version="0.0.5",  # 版本号，数值大的会优先被pip
+    version="0.1.0",  # 版本号，数值大的会优先被pip
     keywords=["pip", "AISimuToolKit"],  # 关键字
     description="ICIP's private utils.",  # 描述
     long_description="ICIP's private utils. The development version will be released when it is sufficiently refined",
     license="MIT Licence",  # 许可证
     url="http://git.cipsup.cn/aisimulationplatform/toolkit/aisimulation",  # 项目相关文件地址，一般是github项目地址即可
     author="Ren & Guo",  # 作者
-    author_email="renmengjie22@mails.ucas.ac.cn",
+    author_email="renmengjie22@mails.ucas.ac.cn,guoshiguang22@mails.ucas.edu.cn",
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
